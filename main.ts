183293e5f92b2f986fbef5a12e529cd0880eb745
mbarbieri785 <85159322+mbarbieri785@users.noreply.github.com>
2022-04-29 09:58:03 +0200

    creato salto

--- main.ts
+++ main.ts
@@ -1,6 +1,16 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (uomo_salto < 1) {
+        uomo.vy = -120
+    }
+})
+let uomo: Sprite = null
+let uomo_salto = 0
 scene.setBackgroundColor(9)
 tiles.setCurrentTilemap(tilemap`level2`)
-let uomo = sprites.create(img`
+let gravita = 250
+let velocita_salto = -120
+uomo_salto = 2
+uomo = sprites.create(img`
     . . . . . . f f f f f f . . . . 
     . . . . f f e e e e f 2 f . . . 
     . . . f f e e e e f 2 2 2 f . . 
@@ -18,5 +28,13 @@ let uomo = sprites.create(img`
     . . . . . . f f f f f f . . . . 
     . . . . . . . f f f . . . . . . 
     `, SpriteKind.Player)
+uomo.setStayInScreen(true)
 controller.moveSprite(uomo, 100, 0)
-uomo.ay = 100
+uomo.setPosition(3, 62)
+uomo.ay = gravita
+scene.cameraFollowSprite(uomo)
+game.onUpdate(function () {
+    if (uomo.isHittingTile(CollisionDirection.Bottom)) {
+        uomo_salto = 0
+    }
+})
